b16200b29f3c7b53c38fe5c16562156d677f3544
Jacques-Etienne Beaudet <jebeaudet@gmail.com>
2017-01-03 09:04:37 -0500

    Added logging

--- ebox.py
+++ ebox.py
@@ -1,17 +1,22 @@
 from bs4 import BeautifulSoup
 import httplib, urllib
 import boto3
+import sys
+import logging
 
 ses = boto3.client('ses')
 
-email_from = ""
+email_from = 'ebox@patate.xyz'
 email_subject = 'Your available usage is down to '
 email_body = 'Your available usage is down to '
 
 def getEboxUsage(event, context):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
     success = False
     code = event["code"]
     email_to = event["email"]
+    logger.info('Starting script for account "%s" and email "%s".', code, email_to)
     params = urllib.urlencode({'actions': 'list', 'DELETE_lng': 'en', 'lng': 'en','code': code})
     headers = {"Content-type": "application/x-www-form-urlencoded"}
     conn = httplib.HTTPConnection("conso.ebox.ca")
@@ -24,11 +29,11 @@ def getEboxUsage(event, context):
         if div.b and "Available" in div.b.string:
             remaining_str = div.contents[1][:-1]
             remaining = float(remaining_str)
-            success = True
+            logger.info('%s GB remaining for account "%s".', remaining, code)
             if remaining < 30:
                 try:
-                    ses.send_email(ReturnPath = email_from, Source = email_from, Destination={'ToAddresses': [email_to]},Message={'Subject': {'Data': email_subject + remaining_str + 'GB for account ' + code},'Body': {'Text': {'Data': email_body + remaining_str + 'GB for account ' + code + '. http://conso.ebox.ca/'}}})
+                    ses.send_email(ReturnPath = email_from, Source = email_from, Destination={'ToAddresses': [email_to]}, Message={'Subject': {'Data': email_subject + remaining_str + 'GB for account ' + code},'Body': {'Text': {'Data': email_body + remaining_str + 'GB for account ' + code + '. http://conso.ebox.ca/'}}})
+                    success = True
                 except Exception, e:
                     print repr(e)
-        
     return success
